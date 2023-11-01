f4d4e85bf7ce6f02e4d68677a62beff7737af5f4
prosiscky <legendaryprosperity001@gmail.com>
2023-11-01 22:32:48 +0100

    file for our data model(s)

---
+++ flask_app/app/models.py
@@ -0,0 +1,22 @@
+# app/models.py
+
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable=False)
+    expensename = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"[{self.expensename}  {self.amount}]"
